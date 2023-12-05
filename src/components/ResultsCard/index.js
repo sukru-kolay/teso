dbd67a613bb251f047de8c9e709abea7fd36f1ab
sukrukolay <sukru.kolay1907@gmail.com>
2023-12-05 22:06:42 +0300

    searchbox

---
+++ src/components/ResultsCard/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ResultsCard = () => {
+  return (
+    <div>
+      <p>Result 1</p>
+      <p>Result 2</p>
+      <p>Result 3</p>
+    </div>
+  );
+};
+
+export default ResultsCard;
